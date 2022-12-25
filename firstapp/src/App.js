703feea44a9de50a4ddda8427e78c1f25cb04813
Altaf Hussain <altafsky17@gmail.com>
2022-12-25 13:08:26 +0530

    new

---
+++ firstapp/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FirstComponents from './Components/FirstComponents';
+
+function App() {
+  return (
+    <div className="App">
+     <FirstComponents/>
+    </div>
+  );
+}
+
+export default App;
---
+++ firstapp/src/Components/FirstComponents.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FirstComponents = () => {
+    return (
+        <React.Fragment>
+            <h1>Bismillah</h1>
+        </React.Fragment>
+    )
+}
+
+export default FirstComponents;
